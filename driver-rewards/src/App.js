8f9c9e887b278b05c7a4c9b61d1341c45d0f72ab
sampazdan <sampazdan@gmail.com>
2023-02-02 15:59:14 -0500

    Initial commit

---
+++ driver-rewards/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+            Render landing page
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
