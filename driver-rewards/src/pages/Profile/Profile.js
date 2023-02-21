4cdfc9466b799fdae274d00a4c592df883da58b3
Andrew Edward Jensen <aejense@clemson.edu>
2023-02-21 20:55:37 +0000

    Manually uploading updated Profile.js

--- driver-rewards/src/pages/Profile/Profile.js
+++ driver-rewards/src/pages/Profile/Profile.js
@@ -3,13 +3,31 @@ import Cookies from "js-cookie";
 
 const Profile = () => {
     
+    const name = Cookies.get('TruckName')
+    const user = Cookies.get('TruckUser')
+    const email = Cookies.get('TruckEmail')
+    const birthday = Cookies.get('TruckBirthday')
+    const gender = Cookies.get('TruckGender')
 
+    const [currUser, setCurrUser] = React.useState(user)
 
     return (
         <div>
-            <button>Profile page goes here</button>
-           
-            
+            Name: {name}<br></br>
+            Username: {user}<br></br>
+            Email: {email}<br></br>
+            DOB: {birthday}<br></br>
+            Gender: {gender}<br></br>
+
+            <button onClick={(e) => {
+                Cookies.set('TruckSession', '')
+                Cookies.set('TruckName', '')
+                Cookies.set('TruckUsername', '')
+                Cookies.set('TruckBirthday', '')
+                Cookies.set('TruckGender', '')
+                setCurrUser('')
+                window.location.reload()
+            }}><br/><br/>Logout</button>
         </div>
     )
 }
