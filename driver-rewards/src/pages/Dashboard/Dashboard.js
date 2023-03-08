18fee14d72499822e993f4db01ebd78172efe331
Ryan Garcia <rjgarci@clemson.edu>
2023-03-08 05:01:56 +0000

    Updated Dashboard.js

--- driver-rewards/src/pages/Dashboard/Dashboard.js
+++ driver-rewards/src/pages/Dashboard/Dashboard.js
@@ -1,21 +1,35 @@
 import React from 'react'
 import Cookies from "js-cookie";
-import * as authtools from "../../authtools";
+import authtools from "../../authtools";
 import axios from "axios";
+import SideMenu from '../../components/SideMenu/SideMenu'
 
 const Dashboard = () => {
 
-    const user = Cookies.get('TruckUser')
+    // const request = {
+    //     username: Cookies.get('TruckName')
+    // }
 
-    const [currUser, setCurrUser] = React.useState(user)
+    //console.log("REQUEST = " + JSON.stringify(request))
+
+    // <SideMenu></SideMenu>
+
+    // const user = authtools.callApi("GET", "user", request)
+    const user = authtools.getUser(Cookies.get('TruckName'))
+
+    console.log("USER = " + user)
+
+    //const [currUser, setCurrUser] = React.useState(user)
 
     return (
+
         <div>
-            Hello {currUser}
+            {/*<SideMenu></SideMenu>*/}
+            Hello {user}
             <button onClick={(e) => {
                 Cookies.set('TruckSession', '')
                 Cookies.set('TruckName', '')
-                setCurrUser('')
+                //setCurrUser('')
                 window.location.reload()
             }}><br/><br/>Logout</button>
         </div>
