a2d9014ae5641b011a1d0bdd090a2d74d85289b8
BlakeDavis34 <bad2@clemson.edu>
2023-03-14 21:10:16 -0400

    Adding Graph
    Graph added, merging conflicts

--- driver-rewards/src/Register.js
+++ driver-rewards/src/Register.js
@@ -25,10 +25,6 @@ const Register = () => {
                 email: <input type="text" value={email} onChange={event => setEmail(event.target.value)} /> <br/>
                 username: <input type="text" value={username} onChange={event => setUsername(event.target.value)} /> <br/>
                 password: <input type="password" value={password} onChange={event => setPassword(event.target.value)} /> <br/>
-                address: <input type="text" value={address} onChange={event => setAddress(event.target.value)} /> <br/>
-                birthday: <input type="text" value={birthday} onChange={event => setBirthday(event.target.value)} /> <br/>
-                gender: <input type="text" value={gender} onChange={event => setGender(event.target.value)} /> <br/>
-                
                 <input type = "submit" value = "Sign up" />
             </form>
         </div>
--- driver-rewards/src/pages/Dashboard/Dashboard.js
+++ driver-rewards/src/pages/Dashboard/Dashboard.js
@@ -1,42 +1,22 @@
-import React from 'react'
-import Cookies from "js-cookie";
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
-import * as authtools from "../../authtools";
-import axios from "axios";
+import React, {useEffect, useState} from 'react'
+import authtools from "../../authtools";
+import Loading from "../../components/Loading/loading";
+import utils from "../../utils"
 
 const Dashboard = () => {
 
-    const user = Cookies.get('TruckUser')
+
+    const [user, setUser] = useState({})
+    const [loading, setLoading] = useState(true)
 
     const [currUser, setCurrUser] = React.useState(user)
 
     return (
+        
         <div>
-            Hello {currUser} 
-            <Tabs isFitted variant='soft-rounded' colorScheme='green'>
-            <TabList mb='1em'>
-                <Tab>Activities</Tab>
-                <Tab>Catalog</Tab>
-                <Tab>Points</Tab>
-            </TabList>
-            <TabPanels>
-                <TabPanel>
-                <p>List of Activities:</p>
-                </TabPanel>
-                <TabPanel>
-                <p>Catalog:</p>
-                </TabPanel>
-                <TabPanel>
-                <p>History of {currUser} Points:</p>
-                </TabPanel>
-            </TabPanels>
-            </Tabs>
-            <button onClick={(e) => {
-                Cookies.set('TruckSession', '')
-                Cookies.set('TruckName', '')
-                setCurrUser('')
-                window.location.reload()
-            }}><br/><br/>Logout</button>
+            Hello {user.username}
+            <br/>
+            User is {JSON.stringify(user)}
         </div>
     )
 }
