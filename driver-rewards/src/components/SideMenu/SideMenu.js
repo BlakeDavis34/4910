c360a4b3901ffd68baa25cd8ca71daa1fc6695db
Ryan Garcia <rjgarci@clemson.edu>
2023-03-15 19:39:38 +0000

    Updated SideMenu.js for Sprint 6

--- driver-rewards/src/components/SideMenu/SideMenu.js
+++ driver-rewards/src/components/SideMenu/SideMenu.js
@@ -1,148 +1,80 @@
+import React, { useState } from 'react'
 import {
-    IconButton,
-    Box,
-    CloseButton,
     Flex,
-    Icon,
-    useColorModeValue,
-    Link,
-    Drawer,
-    DrawerContent,
     Text,
-    useDisclosure,
-    BoxProps,
-    FlexProps,
-  } from '@chakra-ui/react';
-  import {
-    FiHome,
-    FiTrendingUp,
-    FiCompass,
-    FiStar,
-    FiSettings,
+    IconButton,
+    Divider,
+    Avatar,
+    Heading
+} from '@chakra-ui/react'
+import {
     FiMenu,
-  } from 'react-icons/fi';
-  import { IconType } from 'react-icons';
-  import { ReactText } from 'react';
-  
-  
-  const LinkItems = [ 
-    { name: 'Dashboard', FiHome },
-    { name: 'Activities', FiTrendingUp },
-    { name: 'Profile',  FiCompass },
-    { name: 'Catalog',  FiStar },
-    { name: 'Settings', FiSettings },
-  ];
-  
-  export default function SimpleSidebar({ children }) {
-    const { isOpen, onOpen, onClose } = useDisclosure();
+    FiHome,
+    FiCalendar,
+    FiUser,
+    FiDollarSign,
+    FiBriefcase,
+    FiSettings
+} from 'react-icons/fi'
+import { IoPawOutline } from 'react-icons/io5'
+import NavItem from '../NavItem'
+
+export default function Sidebar() {
+    const [navSize, changeNavSize] = useState("large")
     return (
-      <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
-        <SidebarContent
-          onClose={() => onClose}
-          display={{ base: 'none', md: 'block' }}
-        />
-        <Drawer
-          autoFocus={false}
-          isOpen={isOpen}
-          placement="left"
-          onClose={onClose}
-          returnFocusOnClose={false}
-          onOverlayClick={onClose}
-          size="full">
-          <DrawerContent>
-            <SidebarContent onClose={onClose} />
-          </DrawerContent>
-        </Drawer>
-        {/* mobilenav */}
-        <MobileNav display={{ }} onOpen={onOpen} />
-        <Box ml={{  }} p="4">
-          {children}
-        </Box>
-      </Box>
-    );
-  }
-  
- 
-  
-  const SidebarContent = ({ onClose, ...rest }) => {
-    return (
-      <Box
-        bg={useColorModeValue('white', 'gray.900')}
-        borderRight="1px"
-        borderRightColor={useColorModeValue('gray.200', 'gray.700')}
-        w={{ base: 'full', md: 60 }}
-        pos="fixed"
-        h="full"
-        {...rest}>
-        <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
-          <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
-            Logo
-          </Text>
-          <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
-        </Flex>
-        {LinkItems.map((link) => (
-          <NavItem key={link.name} icon={link.icon}>
-            {link.name}
-          </NavItem>
-        ))}
-      </Box>
-    );
-  };
- 
-  const NavItem = ({ icon, children, ...rest }) => {
-    return (
-      <Link href="#" style={{ textDecoration: 'none' }} _focus={{ boxShadow: 'none' }}>
         <Flex
-          align="center"
-          p="4"
-          mx="4"
-          borderRadius="lg"
-          role="group"
-          cursor="pointer"
-          _hover={{
-            bg: 'cyan.400',
-            color: 'white',
-          }}
-          {...rest}>
-          {icon && (
-            <Icon
-              mr="4"
-              fontSize="16"
-              _groupHover={{
-                color: 'white',
-              }}
-              as={icon}
-            />
-          )}
-          {children}
+            pos="sticky"
+            left="5"
+            h="95vh"
+            marginTop="2.5vh"
+            boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
+            borderRadius={navSize == "small" ? "15px" : "30px"}
+            w={navSize == "small" ? "75px" : "200px"}
+            flexDir="column"
+            justifyContent="space-between"
+        >
+            <Flex
+                p="5%"
+                flexDir="column"
+                w="100%"
+                alignItems={navSize == "small" ? "center" : "flex-start"}
+                as="nav"
+            >
+                <IconButton
+                    background="none"
+                    mt={5}
+                    _hover={{ background: 'none' }}
+                    icon={<FiMenu />}
+                    onClick={() => {
+                        if (navSize == "small")
+                            changeNavSize("large")
+                        else
+                            changeNavSize("small")
+                    }}
+                />
+                <NavItem navSize={navSize} icon={FiHome} title="Dashboard" description="This is the description for the dashboard." />
+                <NavItem navSize={navSize} icon={FiCalendar} title="Activities"/>
+                <NavItem navSize={navSize} icon={FiUser} title="Groups" />
+                <NavItem navSize={navSize} icon={IoPawOutline} title="Notifications" />
+                <NavItem navSize={navSize} icon={FiSettings} title="Settings" />
+            </Flex>
+
+            <Flex
+                p="5%"
+                flexDir="column"
+                w="100%"
+                alignItems={navSize == "small" ? "center" : "flex-start"}
+                mb={4}
+            >
+                <Divider display={navSize == "small" ? "none" : "flex"} />
+                <Flex mt={4} align="center">
+                    <Avatar size="sm" src="avatar-1.jpg" />
+                    <Flex flexDir="column" ml={4} display={navSize == "small" ? "none" : "flex"}>
+                        <Heading as="h3" size="sm">Sylwia Weller</Heading>
+                        <Text color="gray">Admin</Text>
+                    </Flex>
+                </Flex>
+            </Flex>
         </Flex>
-      </Link>
-    );
-  };
-  
-  
-  const MobileNav = ({ onOpen, ...rest }) => {
-    return (
-      <Flex
-        ml={{ base: 0, md: 60 }}
-        px={{ base: 4, md: 24 }}
-        height="20"
-        alignItems="center"
-        bg={useColorModeValue('white', 'gray.900')}
-        borderBottomWidth="1px"
-        borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
-        justifyContent="flex-start"
-        {...rest}>
-        <IconButton
-          variant="outline"
-          onClick={onOpen}
-          aria-label="open menu"
-          icon={<FiMenu />}
-        />
-  
-        <Text fontSize="2xl" ml="8" fontFamily="monospace" fontWeight="bold">
-          Logo
-        </Text>
-      </Flex>
-    );
-  };
+    )
+}
